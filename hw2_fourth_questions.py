3f73941733e30b31d4b3f5a259f4dce9763b0757
Tizian Schenk <tizianschenk07@gmail.com>
2025-10-01 11:28:51 +0200

    instructions for ex4

---
+++ hw2_fourth_questions.py
@@ -0,0 +1,16 @@
+###############
+# Use the data in covid.csv for this exercise
+#
+# 10) In a separate file, write a piece of code that
+# loads the covid.csv file and prints the list of countries
+#  and the total average of death/confirmed among those countries
+# for those countries that have more than 500, 1000 and 5000
+# active cases respectively.
+# Follow DRY principles in order to complete this exercise.
+#
+#
+# #
+import pandas as pd
+
+covid = pd.read_csv("covid.csv")
+print(covid.hea())
