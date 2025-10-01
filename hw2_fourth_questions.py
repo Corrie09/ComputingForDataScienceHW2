31b3a022d03162caa4eb9579a9ff80b453a62ec8
Tizian Schenk <tizianschenk07@gmail.com>
2025-10-01 11:50:50 +0200

    fourth question done

--- hw2_fourth_questions.py
+++ hw2_fourth_questions.py
@@ -9,8 +9,50 @@
 # Follow DRY principles in order to complete this exercise.
 #
 #
-# #
+
+
 import pandas as pd
 
 covid = pd.read_csv("covid.csv")
-print(covid.hea())
+print(covid.head())
+
+
+def avg_death_of_all_country_confirmed_above_threshold(threshold):
+    filtered = covid[covid["Active"] > threshold]
+    result = filtered["Deaths"].sum() / filtered["Confirmed"].sum()
+    return result
+
+
+print(
+    "Average death/confirmed for countries with more than 500 active cases:",
+    avg_death_of_all_country_confirmed_above_threshold(500),
+)
+print(
+    "Average death/confirmed for countries with more than 1000 active cases:",
+    avg_death_of_all_country_confirmed_above_threshold(1000),
+)
+print(
+    "Average death/confirmed for countries with more than 5000 active cases:",
+    avg_death_of_all_country_confirmed_above_threshold(5000),
+)
+
+
+# list_of_countries = covid["Country"].unique()
+# covid["avg_death_confirmed"] = covid["Deaths"] / covid["Confirmed"]
+# print(covid.head())
+
+
+"""
+def avg_death_confirmed_above_threshold(threshold):
+    filtered = covid[covid["Active"] > threshold]
+    result = filtered.groupby("Country")["avg_death_confirmed"].mean()
+    return result
+
+
+print("Countries with more than 500 active cases:")
+print(avg_death_confirmed_above_threshold(500))
+print("Countries with more than 1000 active cases:")
+print(avg_death_confirmed_above_threshold(1000))
+print("Countries with more than 5000 active cases:")
+print(avg_death_confirmed_above_threshold(5000))
+"""
